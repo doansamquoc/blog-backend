022533dae5e55b7f0f902d02b8c44de2b0bd7d18
Sam <doansamquoc@gmail.com>
2025-10-25 03:56:59 +0700

    Initial blog-mailer module

---
+++ blog-mailer/src/main/java/com/sam/blog_mailer/BlogMailerApplication.java
@@ -0,0 +1,13 @@
+package com.sam.blog_mailer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMailerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogMailerApplication.class, args);
+	}
+
+}
---
+++ blog-mailer/src/test/java/com/sam/blog_mailer/BlogMailerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.blog_mailer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogMailerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
