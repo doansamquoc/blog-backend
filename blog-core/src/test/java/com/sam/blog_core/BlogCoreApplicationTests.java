4bc2bba8dd0e22ea98fbc850fe2ca7b91e5b03af
Sam <doansamquoc@gmail.com>
2025-10-24 09:40:22 +0700

    Initial module

---
+++ blog-core/src/test/java/com/sam/blog_core/BlogCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.blog_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
