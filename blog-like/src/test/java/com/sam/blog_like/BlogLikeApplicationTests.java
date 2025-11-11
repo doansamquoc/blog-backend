d47ada132642d21853f683ec2b42ba3f08597163
Sam <doansamquoc@gmail.com>
2025-11-11 08:55:36 +0700

    Initial modules

---
+++ blog-like/src/test/java/com/sam/blog_like/BlogLikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.blog_like;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogLikeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ blog-share/src/test/java/com/sam/blog_share/BlogShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.blog_share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
